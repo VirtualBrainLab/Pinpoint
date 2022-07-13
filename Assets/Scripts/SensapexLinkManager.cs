768c73f320b001ddb1d8db93c470351ec12d4f17
Kenneth J. Yang <kjy5@uw.edu>
2022-07-12 17:01:20 -0700

    Converting to probe position (in-progress)

--- Assets/Scripts/SensapexLinkManager.cs
+++ Assets/Scripts/SensapexLinkManager.cs
@@ -15,6 +15,7 @@ public class SensapexLinkManager : MonoBehaviour
     // Components
     private SocketManager connectionManager;
     private TP_TrajectoryPlannerManager tpmanager;
+    private NeedlesTransform neTransform;
 
     private void Awake()
     {
@@ -29,10 +30,13 @@ public class SensapexLinkManager : MonoBehaviour
         connectionManager = new SocketManager(new System.Uri("http://" + _serverIp + ":" + _serverPort));
         connectionManager.Socket.On("connect", () => Debug.Log(connectionManager.Handshake.Sid));
 
+        // Instantiate components
+        neTransform = new NeedlesTransform();
+
         // Register manipulators
         connectionManager.Socket.Emit("register_manipulator", 1);
         connectionManager.Socket.Emit("bypass_calibration", 1);
-        connectionManager.Socket.ExpectAcknowledgement<GetPositionReturnData>(_GetPosCallbackHandler).Emit("get_pos", 1);
+        connectionManager.Socket.ExpectAcknowledgement<GetPositionCallbackParameters>(_GetPosCallbackHandler).Emit("get_pos", 1);
     }
 
     /// <summary>
@@ -42,24 +46,28 @@ public class SensapexLinkManager : MonoBehaviour
     /// Reads the returned data for errors and then prints it back out. Calls another get_pos event at the end.
     /// </para>
     /// <param name="data">Formatted callback parameters for getting position</param>
-    private void _GetPosCallbackHandler(GetPositionReturnData data)
+    private void _GetPosCallbackHandler(GetPositionCallbackParameters data)
     {
         if (data.error == "")
         {
-            Debug.Log("Manipulator " + data.manipulator_id + ": (" + string.Join(", ", data.position) + ")");
+            // Convert to CCF
+            var positionVector = new Vector3(data.position[0], data.position[1], data.position[2]);
+            var ccf = neTransform.ToCCF(positionVector);
+            Debug.Log(ccf);
+            // tpmanager.GetActiveProbeController().SetProbePosition(new ProbeInsertion(ccf.x, ccf.y, ccf.z, data.position[3], 0, 0, 0));
         }
         else
         {
             Debug.LogError(data.error);
         }
 
-        connectionManager.Socket.ExpectAcknowledgement<GetPositionReturnData>(_GetPosCallbackHandler).Emit("get_pos", 1);
+        connectionManager.Socket.ExpectAcknowledgement<GetPositionCallbackParameters>(_GetPosCallbackHandler).Emit("get_pos", 1);
     }
 
     /// <summary>
     /// Returned callback data format from get_pos
     /// </summary>
-    private struct GetPositionReturnData
+    private struct GetPositionCallbackParameters
     {
         public int manipulator_id;
         public float[] position;
