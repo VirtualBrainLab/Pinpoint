38c8bf3787fe181b238993e3d4801a2f03ef951c
Kenneth J. Yang <kjy5@uw.edu>
2022-08-04 10:28:01 -0700

    Correct way to yield for connection

--- Assets/Tests/CommunicationManagerTests.cs
+++ Assets/Tests/CommunicationManagerTests.cs
@@ -17,20 +17,47 @@ namespace Tests
             SceneManager.LoadScene("Scenes/TrajectoryPlanner");
             yield return null;
             _communicationManager = GameObject.Find("SensapexLink").GetComponent<CommunicationManager>();
+            yield return new WaitUntil(_communicationManager.IsConnected);
         }
 
-        // A Test behaves as an ordinary method
-        [Test]
-        public void CommunicationManagerTestsSimplePasses()
+        [TearDown]
+        public void TearDown()
         {
-            const string input = "Hello";
-            Assert.That(input, Is.EqualTo("Hello"));
+            _communicationManager.DisconnectFromServer();
         }
 
+        /// <summary>
+        /// Check if the communication manager is instantiated
+        /// </summary>
         [Test]
-        public void HasAccessToManager()
+        public void TestHasAccessToManager()
         {
             Assert.That(_communicationManager, Is.Not.Null);
+            Assert.True(_communicationManager.IsConnected());
+        }
+
+        /// <summary>
+        /// Test that a success response is received from the server when getting manipulators
+        /// </summary>
+        [UnityTest]
+        public IEnumerator TestGetManipulators()
+        {
+            int[] manipulators = null;
+            string error = null;
+
+            _communicationManager.GetManipulators(
+                returnedManipulators => manipulators = returnedManipulators,
+                returnedError => error = returnedError);
+
+            yield return new WaitWhile(() => manipulators == null && error == null);
+
+            Assert.NotNull(manipulators);
+            Assert.Null(error);
+        }
+
+        [Test]
+        public void TestRegisterAndUnRegister()
+        {
         }
     }
 }
