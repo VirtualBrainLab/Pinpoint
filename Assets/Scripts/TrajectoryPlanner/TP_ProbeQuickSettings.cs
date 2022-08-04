abc6b49e1f63ef89e6a1f6df22dd7427be6de019
Kenneth J. Yang <kjy5@uw.edu>
2022-08-04 16:24:28 -0700

    Added reset Bregma function, connected to button

--- Assets/Scripts/TrajectoryPlanner/TP_ProbeQuickSettings.cs
+++ Assets/Scripts/TrajectoryPlanner/TP_ProbeQuickSettings.cs
@@ -1,4 +1,4 @@
-using System;
+using SensapexLink;
 using TMPro;
 using UnityEngine;
 
@@ -6,31 +6,51 @@ namespace TrajectoryPlanner
 {
     public class TP_ProbeQuickSettings : MonoBehaviour
     {
+        /// <summary>
+        ///     Initialize components
+        /// </summary>
+        private void Awake()
+        {
+            _communicationManager = GameObject.Find("SensapexLink").GetComponent<CommunicationManager>();
+        }
+
         #region Components
 
         [SerializeField] private TMP_Text panelTitle;
         private ProbeManager _probeManager;
+        private CommunicationManager _communicationManager;
 
         #endregion
 
-
         #region Public Methods
 
+        /// <summary>
+        ///     Set active probe (called by TrajectoryPlannerManager)
+        /// </summary>
+        /// <param name="probeManager">Probe Manager of active probe</param>
         public void SetProbeManager(ProbeManager probeManager)
         {
-            if (!gameObject.activeSelf)
-            {
-                gameObject.SetActive(true);
-            }
+            if (!gameObject.activeSelf) gameObject.SetActive(true);
             _probeManager = probeManager;
 
             panelTitle.text = "#" + probeManager.GetID();
             panelTitle.color = probeManager.GetColor();
         }
 
+        /// <summary>
+        ///     Move probe to brain surface and zero out depth
+        /// </summary>
         public void ZeroDepth()
         {
             Debug.Log("Zeroing probe depth");
+        }
+
+        /// <summary>
+        ///     Set current manipulator position to be Bregma and move probe to Bregma
+        /// </summary>
+        public void ResetBregma()
+        {
+            Debug.Log("Reset probe to Bregma");
         }
 
         #endregion
