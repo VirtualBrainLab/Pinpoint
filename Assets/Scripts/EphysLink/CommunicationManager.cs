b0485322bfc30dbcd70788543a55f28dbf3a3d22
Kenneth Yang <kjy5@uw.edu>
2022-09-13 10:12:37 -0700

    Show generic error

--- Assets/Scripts/EphysLink/CommunicationManager.cs
+++ Assets/Scripts/EphysLink/CommunicationManager.cs
@@ -1,5 +1,6 @@
 using System;
 using BestHTTP.SocketIO3;
+using BestHTTP.SocketIO3.Events;
 using UnityEngine;
 
 namespace EphysLink
@@ -69,9 +70,8 @@ namespace EphysLink
             };
             _connectionManager = new SocketManager(new Uri("http://" + ip + ":" + port), options);
             _socket = _connectionManager.Socket;
-
-            // Callback success on connection
-            _socket.Once("connect", () =>
+            
+            _socket.On(SocketIOEventTypes.Connect, () =>
             {
                 Debug.Log("Connected to WebSocket server at " + ip + ":" + port);
                 _serverIp = ip;
@@ -80,15 +80,33 @@ namespace EphysLink
                 _playerPrefs.SaveEphysLinkConnectionData(ip, port);
                 onConnected?.Invoke();
             });
-
-            // Callback error on connection
-            _socket.Once("connect_error", () =>
+            _socket.On(SocketIOEventTypes.Error, () =>
             {
                 var connectionErrorMessage = "Error connecting to WebSocket server at " + ip + ":" + port;
                 Debug.LogWarning(connectionErrorMessage);
                 _isConnected = false;
                 onError?.Invoke(connectionErrorMessage);
             });
+
+            // // Callback success on connection
+            // _socket.Once("connect", () =>
+            // {
+            //     Debug.Log("Connected to WebSocket server at " + ip + ":" + port);
+            //     _serverIp = ip;
+            //     _serverPort = port;
+            //     _isConnected = true;
+            //     _playerPrefs.SaveEphysLinkConnectionData(ip, port);
+            //     onConnected?.Invoke();
+            // });
+            //
+            // // Callback error on connection
+            // _socket.Once("connect_error", () =>
+            // {
+            //     var connectionErrorMessage = "Error connecting to WebSocket server at " + ip + ":" + port;
+            //     Debug.LogWarning(connectionErrorMessage);
+            //     _isConnected = false;
+            //     onError?.Invoke(connectionErrorMessage);
+            // });
             _socket.Once("connect_timeout", () =>
             {
                 var connectionTimeoutMessage = "Connection to WebSocket server at " + ip + ":" + port + " timed out";
