8e8ae9e60b0ac6bda346f9d491462ff3e80433f4
Kenneth Yang <kjy5@uw.edu>
2022-11-22 17:00:14 -0800

    Reset zero coordinate

--- Assets/Scripts/TrajectoryPlanner/AutomaticManipulatorControlHandler.cs
+++ Assets/Scripts/TrajectoryPlanner/AutomaticManipulatorControlHandler.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using EphysLink;
 using TMPro;
 using UnityEngine;
 
@@ -54,6 +55,7 @@ namespace TrajectoryPlanner
         #endregion
 
         private TrajectoryPlannerManager _trajectoryPlannerManager;
+        private CommunicationManager _communicationManager;
 
         #endregion
 
@@ -62,11 +64,16 @@ namespace TrajectoryPlanner
         private ProbeManager _probe1Manager;
         private ProbeManager _probe2Manager;
 
+        private uint _step = 1;
+
         #endregion
+
+        #region Unity
 
         private void Awake()
         {
             _trajectoryPlannerManager = GameObject.Find("main").GetComponent<TrajectoryPlannerManager>();
+            _communicationManager = GameObject.Find("EphysLink").GetComponent<CommunicationManager>();
         }
 
         private void OnEnable()
@@ -76,10 +83,10 @@ namespace TrajectoryPlanner
             _probe2Manager = _trajectoryPlannerManager.GetAllProbes().Find(manager => manager.ManipulatorId == "2");
 
             // Update UI elements
-            
+
             if (_probe1Manager)
             {
-                var probeText = "Probe #"+_probe1Manager.GetID();
+                var probeText = "Probe #" + _probe1Manager.GetID();
                 zeroCoordinateManipulator1ProbeText.text = probeText;
                 gotoManipulator1ProbeText.text = probeText;
                 duraManipulator1ProbeText.text = probeText;
@@ -87,37 +94,81 @@ namespace TrajectoryPlanner
                 zeroCoordinateManipulator1ProbeText.color = _probe1Manager.GetColor();
                 gotoManipulator1ProbeText.color = _probe1Manager.GetColor();
                 duraManipulator1ProbeText.color = _probe1Manager.GetColor();
-                
-                gotoManipulator1XInputField.text =
-                    _probe1Manager.ZeroCoordinateOffset.x.ToString(CultureInfo.CurrentCulture);
-                gotoManipulator1YInputField.text =
-                    _probe1Manager.ZeroCoordinateOffset.y.ToString(CultureInfo.CurrentCulture);
-                gotoManipulator1ZInputField.text =
-                    _probe1Manager.ZeroCoordinateOffset.z.ToString(CultureInfo.CurrentCulture);
-                gotoManipulator1DInputField.text =
-                    _probe1Manager.ZeroCoordinateOffset.w.ToString(CultureInfo.CurrentCulture);
+
+                UpdateManipulator1ZeroCoordinateFields();
             }
 
-            if (_probe2Manager)
+            if (!_probe2Manager) return;
             {
-                var probeText = "Probe #"+_probe2Manager.GetID();
+                var probeText = "Probe #" + _probe2Manager.GetID();
                 zeroCoordinateManipulator2ProbeText.text = probeText;
                 gotoManipulator2ProbeText.text = probeText;
                 duraManipulator2ProbeText.text = probeText;
-                
+
                 zeroCoordinateManipulator2ProbeText.color = _probe2Manager.GetColor();
                 gotoManipulator2ProbeText.color = _probe2Manager.GetColor();
                 duraManipulator2ProbeText.color = _probe2Manager.GetColor();
-                
-                gotoManipulator2XInputField.text =
-                    _probe2Manager.ZeroCoordinateOffset.x.ToString(CultureInfo.CurrentCulture);
-                gotoManipulator2YInputField.text =
-                    _probe2Manager.ZeroCoordinateOffset.y.ToString(CultureInfo.CurrentCulture);
-                gotoManipulator2ZInputField.text =
-                    _probe2Manager.ZeroCoordinateOffset.z.ToString(CultureInfo.CurrentCulture);
-                gotoManipulator2DInputField.text =
-                    _probe2Manager.ZeroCoordinateOffset.w.ToString(CultureInfo.CurrentCulture);
+
+                UpdateManipulator2ZeroCoordinateFields();
             }
         }
+
+        #endregion
+
+        #region Internal UI Functions
+
+        private void UpdateManipulator1ZeroCoordinateFields()
+        {
+            gotoManipulator1XInputField.text =
+                _probe1Manager.ZeroCoordinateOffset.x.ToString(CultureInfo.CurrentCulture);
+            gotoManipulator1YInputField.text =
+                _probe1Manager.ZeroCoordinateOffset.y.ToString(CultureInfo.CurrentCulture);
+            gotoManipulator1ZInputField.text =
+                _probe1Manager.ZeroCoordinateOffset.z.ToString(CultureInfo.CurrentCulture);
+            gotoManipulator1DInputField.text =
+                _probe1Manager.ZeroCoordinateOffset.w.ToString(CultureInfo.CurrentCulture);
+        }
+
+        private void UpdateManipulator2ZeroCoordinateFields()
+        {
+            gotoManipulator2XInputField.text =
+                _probe2Manager.ZeroCoordinateOffset.x.ToString(CultureInfo.CurrentCulture);
+            gotoManipulator2YInputField.text =
+                _probe2Manager.ZeroCoordinateOffset.y.ToString(CultureInfo.CurrentCulture);
+            gotoManipulator2ZInputField.text =
+                _probe2Manager.ZeroCoordinateOffset.z.ToString(CultureInfo.CurrentCulture);
+            gotoManipulator2DInputField.text =
+                _probe2Manager.ZeroCoordinateOffset.w.ToString(CultureInfo.CurrentCulture);
+        }
+
+        private void ResetManipulatorZeroCoordinate(ProbeManager probeManager)
+        {
+            // Check if manipulator is connected
+            if (!probeManager || !probeManager.IsEphysLinkControlled) return;
+
+            // Reset zero coordinate
+            _communicationManager.GetPos(probeManager.ManipulatorId,
+                zeroCoordinate => probeManager.ZeroCoordinateOffset = zeroCoordinate);
+            probeManager.BrainSurfaceOffset = 0;
+
+            // Enable step 2 (if needed)
+            _step = _step == 1 ? 2 : _step;
+        }
+
+        #endregion
+
+        #region UI Functions
+
+        public void ResetManipulator1ZeroCoordinate()
+        {
+            ResetManipulatorZeroCoordinate(_probe1Manager);
+        }
+
+        public void ResetManipulator2ZeroCoordinate()
+        {
+            ResetManipulatorZeroCoordinate(_probe2Manager);
+        }
+
+        #endregion
     }
 }
