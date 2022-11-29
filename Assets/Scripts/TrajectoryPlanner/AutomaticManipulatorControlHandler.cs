8312cc7171a00286aa5658ed0f9fc8b2201550e7
Kenneth Yang <kjy5@uw.edu>
2022-11-29 13:10:06 -0800

    Fixed serialized field names

--- Assets/Scripts/TrajectoryPlanner/AutomaticManipulatorControlHandler.cs
+++ Assets/Scripts/TrajectoryPlanner/AutomaticManipulatorControlHandler.cs
@@ -2,6 +2,7 @@ using System.Globalization;
 using EphysLink;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace TrajectoryPlanner
 {
@@ -11,46 +12,46 @@ namespace TrajectoryPlanner
 
         #region Step 1
 
-        [SerializeField] private CanvasGroup zeroCoordinatePanelCanvasGroup;
-        [SerializeField] private TMP_Text zeroCoordinatePanelText;
-        [SerializeField] private TMP_Text zeroCoordinateManipulator1ProbeText;
-        [SerializeField] private TMP_Text zeroCoordinateManipulator2ProbeText;
+        [SerializeField] private CanvasGroup _zeroCoordinatePanelCanvasGroup;
+        [SerializeField] private TMP_Text _zeroCoordinatePanelText;
+        [SerializeField] private TMP_Text _zeroCoordinateManipulator1ProbeText;
+        [SerializeField] private TMP_Text _zeroCoordinateManipulator2ProbeText;
 
         #endregion
 
         #region Step 2
 
-        [SerializeField] private CanvasGroup gotoPanelCanvasGroup;
-        [SerializeField] private TMP_Text gotoPanelText;
-        [SerializeField] private TMP_Text gotoManipulator1ProbeText;
-        [SerializeField] private TMP_InputField gotoManipulator1XInputField;
-        [SerializeField] private TMP_InputField gotoManipulator1YInputField;
-        [SerializeField] private TMP_InputField gotoManipulator1ZInputField;
-        [SerializeField] private TMP_InputField gotoManipulator1DInputField;
-        [SerializeField] private TMP_Text gotoManipulator2ProbeText;
-        [SerializeField] private TMP_InputField gotoManipulator2XInputField;
-        [SerializeField] private TMP_InputField gotoManipulator2YInputField;
-        [SerializeField] private TMP_InputField gotoManipulator2ZInputField;
-        [SerializeField] private TMP_InputField gotoManipulator2DInputField;
-        [SerializeField] private TMP_Text gotoMoveButtonText;
+        [SerializeField] private CanvasGroup _gotoPanelCanvasGroup;
+        [SerializeField] private TMP_Text _gotoPanelText;
+        [SerializeField] private TMP_Text _gotoManipulator1ProbeText;
+        [SerializeField] private TMP_InputField _gotoManipulator1XInputField;
+        [SerializeField] private TMP_InputField _gotoManipulator1YInputField;
+        [SerializeField] private TMP_InputField _gotoManipulator1ZInputField;
+        [SerializeField] private TMP_InputField _gotoManipulator1DInputField;
+        [SerializeField] private TMP_Text _gotoManipulator2ProbeText;
+        [SerializeField] private TMP_InputField _gotoManipulator2XInputField;
+        [SerializeField] private TMP_InputField _gotoManipulator2YInputField;
+        [SerializeField] private TMP_InputField _gotoManipulator2ZInputField;
+        [SerializeField] private TMP_InputField _gotoManipulator2DInputField;
+        [SerializeField] private TMP_Text _gotoMoveButtonText;
 
         #endregion
 
         #region Step 3
 
-        [SerializeField] private CanvasGroup duraPanelCanvasGroup;
-        [SerializeField] private TMP_Text duraPanelText;
-        [SerializeField] private TMP_Text duraManipulator1ProbeText;
-        [SerializeField] private TMP_Text duraManipulator2ProbeText;
+        [SerializeField] private CanvasGroup _duraPanelCanvasGroup;
+        [SerializeField] private TMP_Text _duraPanelText;
+        [SerializeField] private TMP_Text _duraManipulator1ProbeText;
+        [SerializeField] private TMP_Text _duraManipulator2ProbeText;
 
         #endregion
 
         #region Step 4
 
-        [SerializeField] private CanvasGroup drivePanelCanvasGroup;
-        [SerializeField] private TMP_Text drivePanelText;
-        [SerializeField] private TMP_Text driveStatusText;
-        [SerializeField] private TMP_Text driveButtonText;
+        [SerializeField] private CanvasGroup _drivePanelCanvasGroup;
+        [SerializeField] private TMP_Text _drivePanelText;
+        [SerializeField] private TMP_Text _driveStatusText;
+        [SerializeField] private TMP_Text _driveButtonText;
 
         #endregion
 
@@ -87,13 +88,13 @@ namespace TrajectoryPlanner
             if (_probe1Manager)
             {
                 var probeText = "Probe #" + _probe1Manager.GetID();
-                zeroCoordinateManipulator1ProbeText.text = probeText;
-                gotoManipulator1ProbeText.text = probeText;
-                duraManipulator1ProbeText.text = probeText;
+                _zeroCoordinateManipulator1ProbeText.text = probeText;
+                _gotoManipulator1ProbeText.text = probeText;
+                _duraManipulator1ProbeText.text = probeText;
 
-                zeroCoordinateManipulator1ProbeText.color = _probe1Manager.GetColor();
-                gotoManipulator1ProbeText.color = _probe1Manager.GetColor();
-                duraManipulator1ProbeText.color = _probe1Manager.GetColor();
+                _zeroCoordinateManipulator1ProbeText.color = _probe1Manager.GetColor();
+                _gotoManipulator1ProbeText.color = _probe1Manager.GetColor();
+                _duraManipulator1ProbeText.color = _probe1Manager.GetColor();
 
                 UpdateManipulator1ZeroCoordinateFields();
             }
@@ -101,13 +102,13 @@ namespace TrajectoryPlanner
             if (!_probe2Manager) return;
             {
                 var probeText = "Probe #" + _probe2Manager.GetID();
-                zeroCoordinateManipulator2ProbeText.text = probeText;
-                gotoManipulator2ProbeText.text = probeText;
-                duraManipulator2ProbeText.text = probeText;
+                _zeroCoordinateManipulator2ProbeText.text = probeText;
+                _gotoManipulator2ProbeText.text = probeText;
+                _duraManipulator2ProbeText.text = probeText;
 
-                zeroCoordinateManipulator2ProbeText.color = _probe2Manager.GetColor();
-                gotoManipulator2ProbeText.color = _probe2Manager.GetColor();
-                duraManipulator2ProbeText.color = _probe2Manager.GetColor();
+                _zeroCoordinateManipulator2ProbeText.color = _probe2Manager.GetColor();
+                _gotoManipulator2ProbeText.color = _probe2Manager.GetColor();
+                _duraManipulator2ProbeText.color = _probe2Manager.GetColor();
 
                 UpdateManipulator2ZeroCoordinateFields();
             }
@@ -119,25 +120,25 @@ namespace TrajectoryPlanner
 
         private void UpdateManipulator1ZeroCoordinateFields()
         {
-            gotoManipulator1XInputField.text =
+            _gotoManipulator1XInputField.text =
                 _probe1Manager.ZeroCoordinateOffset.x.ToString(CultureInfo.CurrentCulture);
-            gotoManipulator1YInputField.text =
+            _gotoManipulator1YInputField.text =
                 _probe1Manager.ZeroCoordinateOffset.y.ToString(CultureInfo.CurrentCulture);
-            gotoManipulator1ZInputField.text =
+            _gotoManipulator1ZInputField.text =
                 _probe1Manager.ZeroCoordinateOffset.z.ToString(CultureInfo.CurrentCulture);
-            gotoManipulator1DInputField.text =
+            _gotoManipulator1DInputField.text =
                 _probe1Manager.ZeroCoordinateOffset.w.ToString(CultureInfo.CurrentCulture);
         }
 
         private void UpdateManipulator2ZeroCoordinateFields()
         {
-            gotoManipulator2XInputField.text =
+            _gotoManipulator2XInputField.text =
                 _probe2Manager.ZeroCoordinateOffset.x.ToString(CultureInfo.CurrentCulture);
-            gotoManipulator2YInputField.text =
+            _gotoManipulator2YInputField.text =
                 _probe2Manager.ZeroCoordinateOffset.y.ToString(CultureInfo.CurrentCulture);
-            gotoManipulator2ZInputField.text =
+            _gotoManipulator2ZInputField.text =
                 _probe2Manager.ZeroCoordinateOffset.z.ToString(CultureInfo.CurrentCulture);
-            gotoManipulator2DInputField.text =
+            _gotoManipulator2DInputField.text =
                 _probe2Manager.ZeroCoordinateOffset.w.ToString(CultureInfo.CurrentCulture);
         }
 
