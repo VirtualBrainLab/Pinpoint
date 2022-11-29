ac129ddf35203a3c2de4621cd0d8b509cc86fc9d
Kenneth Yang <kjy5@uw.edu>
2022-11-29 12:01:16 -0800

    tp axis and probe

--- Assets/Scripts/TrajectoryPlanner/AxisControl.cs
+++ Assets/Scripts/TrajectoryPlanner/AxisControl.cs
@@ -1,38 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class AxisControl : MonoBehaviour
 {
-    [SerializeField] GameObject apGO;
-    [SerializeField] GameObject dvGO;
-    [SerializeField] GameObject mlGO;
-    [SerializeField] GameObject depthGO;
+    [FormerlySerializedAs("apGO")] [SerializeField] private GameObject _apGo;
+    [FormerlySerializedAs("dvGO")] [SerializeField] private GameObject _dvGo;
+    [FormerlySerializedAs("mlGO")] [SerializeField] private GameObject _mlGo;
+    [FormerlySerializedAs("depthGO")] [SerializeField] private GameObject _depthGo;
 
     public void SetAxisPosition(Transform transform)
     {
         if (transform == null)
             return;
-        apGO.transform.position = transform.position;
-        dvGO.transform.position = transform.position;
-        mlGO.transform.position = transform.position;
-        depthGO.transform.position = transform.position;
-        depthGO.transform.rotation = transform.rotation;
+        _apGo.transform.position = transform.position;
+        _dvGo.transform.position = transform.position;
+        _mlGo.transform.position = transform.position;
+        _depthGo.transform.position = transform.position;
+        _depthGo.transform.rotation = transform.rotation;
     }
     public void SetAPVisibility(bool state)
     {
-        apGO.SetActive(state);
+        _apGo.SetActive(state);
     }
     public void SetDVVisibility(bool state)
     {
-        dvGO.SetActive(state);
+        _dvGo.SetActive(state);
     }
     public void SetMLVisibility(bool state)
     {
-        mlGO.SetActive(state);
+        _mlGo.SetActive(state);
     }
     public void SetDepthVisibility(bool state)
     {
-        depthGO.SetActive(state);
+        _depthGo.SetActive(state);
     }
 }
