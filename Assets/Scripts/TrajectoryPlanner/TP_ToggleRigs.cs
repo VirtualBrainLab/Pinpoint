f649bfe625fe512ecd5cdb8f0a4efa490eeda182
Dan Birman <danbirman@gmail.com>
2023-01-05 15:55:57 -0800

    Adding rig persistence across program closing/opening

--- Assets/Scripts/TrajectoryPlanner/TP_ToggleRigs.cs
+++ Assets/Scripts/TrajectoryPlanner/TP_ToggleRigs.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TrajectoryPlanner;
 using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class TP_ToggleRigs : MonoBehaviour
 {
@@ -10,11 +11,21 @@ public class TP_ToggleRigs : MonoBehaviour
     // Exposed the list of rigs
     [FormerlySerializedAs("rigGOs")] [SerializeField] private List<GameObject> _rigGOs;
 
+    // Exposed list of rig UI objects
+    [SerializeField] private GameObject _rigUIParentGO;
+
     // Start is called before the first frame update
     void Start()
     {
-        foreach (GameObject go in _rigGOs)
-            go.SetActive(false);
+        for (int i = 0; i < _rigGOs.Count; i++)
+            if (PlayerPrefs.HasKey("rig" + i))
+            {
+                bool active = PlayerPrefs.GetInt("rig" + i) == 1;
+                _rigGOs[i].SetActive(active);
+                _rigUIParentGO.transform.GetChild(i).gameObject.GetComponent<Toggle>().SetIsOnWithoutNotify(active);
+            }
+            else
+                _rigGOs[i].SetActive(false);
     }
 
     public void ToggleRigVisibility(int rigIdx)
@@ -23,5 +34,11 @@ public class TP_ToggleRigs : MonoBehaviour
         Collider[] colliders = _rigGOs[rigIdx].transform.GetComponentsInChildren<Collider>();
         _tpmanager.UpdateRigColliders(colliders, _rigGOs[rigIdx].activeSelf);
         ColliderManager.CheckForCollisions();
+    }
+
+    private void OnApplicationQuit()
+    {
+        for (int i = 0; i < _rigGOs.Count; i++)
+            PlayerPrefs.SetInt("rig" + i, _rigGOs[i].activeSelf ? 1 : 0);
     }
 }
