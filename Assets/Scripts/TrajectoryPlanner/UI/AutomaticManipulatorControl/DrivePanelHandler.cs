5bfcc65e886bd2bca55523b822ebeb4fe9238e67
Kenneth Yang <kjy5@uw.edu>
2023-01-11 11:21:43 -0800

    Create drive panel + basic script

---
+++ Assets/Scripts/TrajectoryPlanner/UI/AutomaticManipulatorControl/DrivePanelHandler.cs
@@ -0,0 +1,36 @@
+using System;
+using EphysLink;
+using TMPro;
+using UnityEngine;
+
+namespace TrajectoryPlanner.UI.AutomaticManipulatorControl
+{
+    public class DrivePanelHandler : MonoBehaviour
+    {
+        #region Unity
+
+        private void Start()
+        {
+            _manipulatorIDText.text = "Manipulator " + ProbeManager.ManipulatorId;
+            _manipulatorIDText.color = ProbeManager.GetColor();
+        }
+
+        #endregion
+        #region Components
+
+        #region Shared
+
+        public static CommunicationManager CommunicationManager { private get; set; }
+
+        #endregion
+
+        [SerializeField] private TMP_Text _manipulatorIDText;
+        [SerializeField] private TMP_Text _statusText;
+        [SerializeField] private TMP_Text _timeText;
+        [SerializeField] private TMP_Text _buttonText;
+        
+        public ProbeManager ProbeManager { private get; set; }
+
+        #endregion
+    }
+}
