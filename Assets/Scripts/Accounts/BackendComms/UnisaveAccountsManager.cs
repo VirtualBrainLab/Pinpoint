7d928db3dc9326e07bade6b3f0793901c32ab694
Dan Birman <danbirman@gmail.com>
2023-02-28 19:55:02 -0800

    Adding ClearToken calls to better prevent re-login attempts via token when the token is clearly invalid

--- Assets/Scripts/Accounts/BackendComms/UnisaveAccountsManager.cs
+++ Assets/Scripts/Accounts/BackendComms/UnisaveAccountsManager.cs
@@ -123,6 +123,8 @@ public class UnisaveAccountsManager : AccountsManager
 
         ExperimentListChangeEvent.Invoke();
         InsertionListChangeEvent.Invoke();
+
+        _emailLoginForm.ClearToken();
     }
 
 #endregion
