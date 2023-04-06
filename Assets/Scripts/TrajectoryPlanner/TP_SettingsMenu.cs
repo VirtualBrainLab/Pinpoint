d43da9028045819c43fe577a5301e79ef12a8b21
Dan Birman <danbirman@gmail.com>
2023-04-05 18:15:08 -0700

    feat: help text now hides itself automatically after the first time a user opens the menu

--- Assets/Scripts/TrajectoryPlanner/TP_SettingsMenu.cs
+++ Assets/Scripts/TrajectoryPlanner/TP_SettingsMenu.cs
@@ -9,11 +9,29 @@ namespace UITabs
     public class TP_SettingsMenu : MonoBehaviour
     {
         [FormerlySerializedAs("settingsMenuGO")] [SerializeField] private GameObject _settingsMenuGo;
+        [SerializeField] private GameObject _helpText;
+
+        private const string HELP_KEY = "hide-help-at-startup";
+
+        private void Awake()
+        {
+            if (PlayerPrefs.HasKey(HELP_KEY) && PlayerPrefs.GetInt(HELP_KEY) == 1)
+                _helpText.SetActive(false);
+        }
 
         public void ToggleSettingsMenu()
         {
             if (!_settingsMenuGo.activeSelf)
+            {
+                // Hide the help text
                 _settingsMenuGo.SetActive(true);
+
+                if (_helpText.activeSelf)
+                {
+                    _helpText.SetActive(false);
+                    PlayerPrefs.SetInt(HELP_KEY, 1);
+                }
+            }
             else
             {
                 // if the settings menu is active, we want to make sure the user isn't typing before we close the menu
