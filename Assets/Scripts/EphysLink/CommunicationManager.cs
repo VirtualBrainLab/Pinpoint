ea4cc59deaf3a08975d683641fc199bd7dfca98f
Kenneth Yang <82800265+kjy5@users.noreply.github.com>
2023-09-07 10:19:03 -0700

    chore: bump Ephys Link version (#530)

--- Assets/Scripts/EphysLink/CommunicationManager.cs
+++ Assets/Scripts/EphysLink/CommunicationManager.cs
@@ -22,7 +22,7 @@ namespace EphysLink
 
         #region Variables
 
-        public static readonly int[] EPHYS_LINK_MIN_VERSION = { 0, 9, 10 };
+        public static readonly int[] EPHYS_LINK_MIN_VERSION = { 0, 9, 12 };
 
         public static readonly string EPHYS_LINK_MIN_VERSION_STRING = "≥ v" + string.Join(".", EPHYS_LINK_MIN_VERSION);
 
