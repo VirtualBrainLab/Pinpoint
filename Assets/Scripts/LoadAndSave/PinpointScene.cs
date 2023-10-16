9ca57a5eaa63d0b810a8dd3838adf49b8d31fcf2
Dan Birman <danbirman@gmail.com>
2023-10-16 13:20:59 -0700

    feat: getting ready for load/save features

---
+++ Assets/Scripts/LoadAndSave/PinpointScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct PinpointScene
+{
+    public string AtlasName;
+    public string AtlasTransformName;
+
+    public List<string> ProbeInsertions;
+    public List<string> RigParts;
+
+    public string Settings;
+}
