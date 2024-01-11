7aa342063f270868b1990621c1e32562bbd87af3
Dan Birman <danbirman@gmail.com>
2024-01-11 12:13:36 -0800

    chore: missing usings, clarifying Craniotomy

--- Assets/Scripts/Pinpoint/JSON/CraniotomyData.cs
+++ Assets/Scripts/Pinpoint/JSON/CraniotomyData.cs
@@ -1,11 +1,16 @@
+using System;
 using UnityEngine;
 
 /// <summary>
-/// Craniotomy Position is the center point, extent is -Width/2 to +Width/2, etc
+/// Craniotomy Position is the center point, extent is -Width/2 to +Width/2, etc if Rectangle or Width = Height = Diameter
 /// </summary>
+/// 
+
+[Serializable]
 public struct CraniotomyData
 {
     public Vector3 Position;
     public float Width;
     public float Height;
+    public bool Rectangle;
 }
--- Assets/Scripts/Pinpoint/JSON/ManipulatorControllerData.cs
+++ Assets/Scripts/Pinpoint/JSON/ManipulatorControllerData.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 
+[Serializable]
 public struct ManipulatorControllerData
 {
     public string ManipulatorID;
