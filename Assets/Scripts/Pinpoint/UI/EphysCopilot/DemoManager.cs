7cb9d7c904bd0b3ee5bbe256527b93c4d281ac57
Kenneth Yang <kjy5@uw.edu>
2024-06-19 13:34:59 -0700

    WIP compute positions

--- Assets/Scripts/Pinpoint/UI/EphysCopilot/DemoManager.cs
+++ Assets/Scripts/Pinpoint/UI/EphysCopilot/DemoManager.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using EphysLink;
+using Pinpoint.Probes;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -8,45 +11,58 @@ namespace Pinpoint.UI.EphysCopilot
 {
     public class DemoManager : MonoBehaviour
     {
+        #region Constants
+
+        private const float MOVEMENT_SPEED = 1f;
+
+        #endregion
         #region Components
 
         // Existing UI for toggling.
+
         [SerializeField]
         private GameObject _canvasGameObject;
 
+        private readonly HashSet<GameObject> _existingUIGameObjects = new();
+
         // Demo UI.
 
+
         // Camera.
+
         [SerializeField]
         private BrainCameraController _mainCamera;
+
+        // Manipulators.
+        private readonly Dictionary<string, ManipulatorBehaviorController> _manipulators = new();
+        private readonly Dictionary<string, ProbeManager> _probeManagers = new();
 
         #endregion
 
         #region Properties
 
-        private readonly HashSet<GameObject> _existingUIGameObjects = new();
-
         private struct DemoData
         {
-            public string Manipulator1Id;
-            public Positions Manipulator1Positions;
+            public string Id1;
+            public Vector3 Home1;
+            public Vector4 Target1;
 
-            public string Manipulator2Id;
-            public Positions Manipulator2Positions;
+            public string Id2;
+            public Vector3 Home2;
+            public Vector4 Target2;
 
-            public string Manipulator3Id;
-            public Positions Manipulator3Positions;
-
-            [Serializable]
-            public struct Positions
-            {
-                public Vector4 Home;
-                public Vector4 Bregma;
-                public Vector4 Target;
-            }
+            public string Id3;
+            public Vector3 Home3;
+            public Vector4 Target3;
         }
 
         private DemoData _demoData;
+
+        private readonly Dictionary<string, Vector4> _convertedHomePositions = new();
+        private readonly Dictionary<string, Vector4> _convertedBregmaCoordinates = new();
+        private readonly Dictionary<string, Vector4> _convertedInsertionCoordinates = new();
+        private Dictionary<string, Vector4> _convertedDuraCoordinates = new();
+        private readonly Dictionary<string, Vector4> _convertedTargetCoordinates = new();
 
         #endregion
 
@@ -91,9 +107,128 @@ namespace Pinpoint.UI.EphysCopilot
             _mainCamera.transform.rotation = Quaternion.Euler(180, -180, -180);
 
             // Read demo data.
-            var demoDataPath = Path.Combine(Application.streamingAssetsPath, "DemoData.json");
-            var demoDataJson = File.ReadAllText(demoDataPath);
-            _demoData = JsonUtility.FromJson<DemoData>(demoDataJson);
+            _demoData = JsonUtility.FromJson<DemoData>(
+                File.ReadAllText(Path.Combine(Application.streamingAssetsPath, "DemoData.json"))
+            );
+
+            // Get manipulators.
+            foreach (
+                var probeManager in ProbeManager.Instances.Where(probeManager =>
+                    probeManager.IsEphysLinkControlled
+                )
+            )
+            {
+                _manipulators.Add(
+                    probeManager.ManipulatorBehaviorController.ManipulatorID,
+                    probeManager.ManipulatorBehaviorController
+                );
+                _probeManagers.Add(
+                    probeManager.ManipulatorBehaviorController.ManipulatorID,
+                    probeManager
+                );
+            }
+
+            // Compute Manipulator 1 positions.
+            if (_manipulators.ContainsKey(_demoData.Id1))
+            {
+                _convertedHomePositions.Add(
+                    _demoData.Id1,
+                    _manipulators[_demoData.Id1]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(_demoData.Home1)
+                );
+                _convertedBregmaCoordinates.Add(
+                    _demoData.Id1,
+                    _manipulators[_demoData.Id1]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(Vector3.zero)
+                );
+                _convertedInsertionCoordinates.Add(
+                    _demoData.Id1,
+                    _manipulators[_demoData.Id1]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(
+                            new Vector3(_demoData.Target1.x, _demoData.Target1.y, 3)
+                        )
+                );
+                _convertedTargetCoordinates.Add(
+                    _demoData.Id1,
+                    _manipulators[_demoData.Id1]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(
+                            new Vector3(
+                                _demoData.Target1.x,
+                                _demoData.Target1.y,
+                                _demoData.Target1.z - _demoData.Target1.w
+                            )
+                        )
+                );
+            }
+
+            // Compute Manipulator 2 positions.
+            if (_manipulators.ContainsKey(_demoData.Id2))
+            {
+                _convertedHomePositions.Add(
+                    _demoData.Id2,
+                    _manipulators[_demoData.Id2]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(_demoData.Home2)
+                );
+                _convertedBregmaCoordinates.Add(
+                    _demoData.Id2,
+                    _manipulators[_demoData.Id2]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(Vector3.zero)
+                );
+                _convertedInsertionCoordinates.Add(
+                    _demoData.Id2,
+                    _manipulators[_demoData.Id2]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(
+                            new Vector3(_demoData.Target2.x, _demoData.Target2.y, 3)
+                        )
+                );
+                _convertedTargetCoordinates.Add(
+                    _demoData.Id2,
+                    _manipulators[_demoData.Id2]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(
+                            new Vector3(
+                                _demoData.Target2.x,
+                                _demoData.Target2.y,
+                                _demoData.Target2.z - _demoData.Target2.w
+                            )
+                        )
+                );
+            }
+
+            // Compute Manipulator 3 positions.
+            if (_manipulators.ContainsKey(_demoData.Id3))
+            {
+                _convertedHomePositions.Add(
+                    _demoData.Id3,
+                    _manipulators[_demoData.Id3]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(_demoData.Home3)
+                );
+                _convertedBregmaCoordinates.Add(
+                    _demoData.Id3,
+                    _manipulators[_demoData.Id3]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(Vector3.zero)
+                );
+                _convertedInsertionCoordinates.Add(
+                    _demoData.Id3,
+                    _manipulators[_demoData.Id3]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(
+                            new Vector3(_demoData.Target3.x, _demoData.Target3.y, 3)
+                        )
+                );
+                _convertedTargetCoordinates.Add(
+                    _demoData.Id3,
+                    _manipulators[_demoData.Id3]
+                        .ConvertInsertionAPMLDVToManipulatorPosition(
+                            new Vector3(
+                                _demoData.Target3.x,
+                                _demoData.Target3.y,
+                                _demoData.Target3.z - _demoData.Target3.w
+                            )
+                        )
+                );
+            }
+
+            // Start demo.
+            MoveToHome();
         }
 
         public void StopDemo()
@@ -108,6 +243,58 @@ namespace Pinpoint.UI.EphysCopilot
 
             // Reset camera.
             _mainCamera.SetZoom(5);
+        }
+
+        #endregion
+
+        #region Demo Functions
+
+        public void MoveToHome()
+        {
+            CommunicationManager.Instance.SetCanWrite(
+                new CanWriteRequest(_demoData.Id1, true, 1),
+                _ =>
+                {
+                    CommunicationManager.Instance.GotoPos(
+                        new GotoPositionRequest(
+                            _demoData.Id1,
+                            _convertedHomePositions[_demoData.Id1],
+                            MOVEMENT_SPEED
+                        ),
+                        _ =>
+                        {
+                            CommunicationManager.Instance.GotoPos(
+                                new GotoPositionRequest(
+                                    _demoData.Id1,
+                                    _convertedBregmaCoordinates[_demoData.Id1],
+                                    MOVEMENT_SPEED
+                                ),
+                                _ =>
+                                {
+                                    CommunicationManager.Instance.GotoPos(
+                                        new GotoPositionRequest(
+                                            _demoData.Id1,
+                                            _convertedInsertionCoordinates[_demoData.Id1],
+                                            MOVEMENT_SPEED
+                                        ),
+                                        _ =>
+                                        {
+                                            CommunicationManager.Instance.GotoPos(
+                                                new GotoPositionRequest(
+                                                    _demoData.Id1,
+                                                    _convertedTargetCoordinates[_demoData.Id1],
+                                                    MOVEMENT_SPEED
+                                                ),
+                                                _ => MoveToHome()
+                                            );
+                                        }
+                                    );
+                                }
+                            );
+                        }
+                    );
+                }
+            );
         }
 
         #endregion
