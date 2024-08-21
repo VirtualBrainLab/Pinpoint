e163c573af939b17fdd6033d5fe84cfbf9413b50
Kenneth Yang <kjy5@uw.edu>
2024-08-21 15:52:48 -0700

    Add inspector state

---
+++ Assets/Scripts/UI/States/InspectorStackState.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using Core.Util;
+using Unity.Properties;
+using UnityEngine;
+
+namespace UI.States
+{
+    [CreateAssetMenu]
+    public class InspectorStackState : ResettingScriptableObject
+    {
+        #region Properties
+
+        // Stack enabled state.
+        [CreateProperty]
+        public bool IsEnabled => ProbeManager.ActiveProbeManager;
+
+        #endregion
+    }
+}
