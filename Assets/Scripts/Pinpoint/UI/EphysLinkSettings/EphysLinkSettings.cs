c851eb2aae93fa957b203ac54acfa27e1a360b8d
Kenneth Yang <kjy5@uw.edu>
2025-04-09 16:04:38 -0700

    chore: bump Ephys Link

--- Assets/Scripts/Pinpoint/UI/EphysLinkSettings/EphysLinkSettings.cs
+++ Assets/Scripts/Pinpoint/UI/EphysLinkSettings/EphysLinkSettings.cs
@@ -20,7 +20,7 @@ namespace Pinpoint.UI.EphysLinkSettings
     {
         #region Constants
 
-        private const string EPHYS_LINK_NAME = "EphysLink-v2.0.0rc0";
+        private const string EPHYS_LINK_NAME = "EphysLink-v2.0.2";
 
         private static string EphysLinkExePath =>
             Path.Combine(
