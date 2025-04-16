24ead99babe07688e047597f688e65fe05df7da9
Kenneth Yang <kjy5@uw.edu>
2025-04-16 16:59:55 -0700

    Migrate to unified Sensapex uMp

--- Assets/Scripts/Pinpoint/UI/EphysLinkSettings/EphysLinkSettings.cs
+++ Assets/Scripts/Pinpoint/UI/EphysLinkSettings/EphysLinkSettings.cs
@@ -109,7 +109,6 @@ namespace Pinpoint.UI.EphysLinkSettings
 
         public void OnTypeChanged(int type)
         {
-            print("Type changed to " + type);
             // Show/hide extra groups based on connection type
             _existingServerGroup.SetActive(type == _manipulatorTypeDropdown.options.Count - 2);
             _proxyServerGroup.SetActive(type == _manipulatorTypeDropdown.options.Count - 1);
@@ -117,7 +116,7 @@ namespace Pinpoint.UI.EphysLinkSettings
             _launchEphysLinkButton.gameObject.SetActive(
                 type < _manipulatorTypeDropdown.options.Count - 2
             );
-            _pathfinderPortInputField.gameObject.SetActive(type == 2);
+            _pathfinderPortInputField.gameObject.SetActive(type == 1);
 
             // Save settings
             Settings.EphysLinkManipulatorType = type;
@@ -225,14 +224,8 @@ namespace Pinpoint.UI.EphysLinkSettings
         /// </summary>
         public void OnLaunchEphysLinkPressed()
         {
-            // Parse manipulator type string arg (invariant: custom connection should never happen).
-            var manipulatorTypeString = _manipulatorTypeDropdown.value switch
-            {
-                1 => "ump-3",
-                2 => "pathfinder-mpm",
-                3 => "new-scale",
-                _ => "ump-4"
-            };
+            // Parse manipulator type string arg.
+            var manipulatorTypeString = _manipulatorTypeDropdown.value == 0 ? "ump" : "pathfinder-mpm";
 
             // Make args string (ignore updates, select type).
             var args = $"-i -t {manipulatorTypeString}";
