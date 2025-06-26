3fc08c75c5f46932e798dec887ac811d39546838
Kenneth Yang <kjy5@uw.edu>
2025-06-26 15:44:25 -0700

    MVVM cycle for opening and closing the left side panel

--- Assets/Scripts/UI/ViewModels/MainViewModel.cs
+++ Assets/Scripts/UI/ViewModels/MainViewModel.cs
@@ -1,9 +1,59 @@
+using UI.Models;
+using UI.Services;
 using Unity.AppUI.MVVM;
+using Unity.AppUI.Redux;
+using UnityEngine;
 
 namespace UI.ViewModels
 {
     public class MainViewModel: ObservableObject
     {
-        
+        #region Services
+
+        private readonly ILocalStorageService _localStorageService;
+        private readonly IStoreService _storeService;
+        private readonly Unsubscriber _unsubscribe;
+        private const string SLICE_NAME = "MainState";
+
+        #endregion
+        #region Commands
+
+        public RelayCommand ToggleSidePanelLeftCommand { get; }
+
+        #endregion
+
+        #region Properties
+
+        private bool _isSidePanelLeftOpen = true;
+
+        public bool IsSidePanelLeftOpen
+        {
+            get => _isSidePanelLeftOpen;
+            private set => SetProperty(ref _isSidePanelLeftOpen, value);
+        }
+
+        #endregion
+
+        public MainViewModel(ILocalStorageService localStorageService, IStoreService storeService)
+        {
+            // Services.
+            _localStorageService = localStorageService;
+            _storeService = storeService;
+            
+            // Commands.
+            ToggleSidePanelLeftCommand = new RelayCommand(ToggleSidePanelLeft);
+            
+            // State.
+            var initialState = _localStorageService.GetValue(SLICE_NAME, new MainState());
+        }
+
+        #region Command Implementation
+
+        private void ToggleSidePanelLeft()
+        {
+            IsSidePanelLeftOpen = !IsSidePanelLeftOpen;
+        }
+
+        #endregion
     }
 }
--- Assets/Scripts/UI/Views/MainView.cs
+++ Assets/Scripts/UI/Views/MainView.cs
@@ -1,14 +1,15 @@
 using System;
+using System.ComponentModel;
 using System.Linq;
 using UI.ViewModels;
 using UnityEngine.UIElements;
 
 namespace UI.Views
 {
-    public class MainView: VisualElement
+    public class MainView : VisualElement
     {
         #region Component References
-        
+
         private readonly VisualElement _sidePanelLeft;
         private readonly VisualElement _sidePanelRight;
 
@@ -29,26 +30,56 @@ namespace UI.Views
             // Instantiate the UI document.
             var document = PinpointAppBuilder.Instance.MainUIDocument;
             document.CloneTree(this);
-            
+
             // Set the flex grow to fill the parent container.
             style.flexGrow = 1;
-            
+
             // Get view bindings.
             _viewBindings = Children().First().dataSource as MainViewBindings;
             if (_viewBindings == null)
             {
                 throw new MissingFieldException("MainViewBindings was not found.");
             }
-            
+
+            // Get view model.
+            _viewModel = mainViewModel;
+            mainViewModel.PropertyChanged += OnPropertyChanged;
+
             // Register component references.
             _sidePanelLeft = this.Q<VisualElement>("side-panel__left");
             _sidePanelRight = this.Q<VisualElement>("side-panel__right");
             _sidePanelLeftToggle = _sidePanelLeft.Q<Button>("side-panel__toggle");
             _sidePanelRightToggle = _sidePanelRight.Q<Button>("side-panel__toggle");
-            
+
             // Register callbacks.
-            // TODO
+            _sidePanelLeftToggle.clicked += () =>
+                mainViewModel.ToggleSidePanelLeftCommand.Execute();
         }
 
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(MainViewModel.IsSidePanelLeftOpen):
+                    ToggleSidePanelLeft();
+                    break;
+            }
+        }
+
+        #region Functions
+
+        private void ToggleSidePanelLeft()
+        {
+            _sidePanelLeft.ToggleInClassList("side-panel--close");
+            _viewBindings.SidePanelLeftPickingMode = _viewModel.IsSidePanelLeftOpen
+                ? PickingMode.Position
+                : PickingMode.Ignore;
+            _viewBindings.SidePanelLeftToggleText = _viewModel.IsSidePanelLeftOpen ? "◀" : "▶";
+            _viewBindings.SidePanelLeftItemDisplayStyle = _viewModel.IsSidePanelLeftOpen
+                ? DisplayStyle.Flex
+                : DisplayStyle.None;
+        }
+
+        #endregion
     }
 }
