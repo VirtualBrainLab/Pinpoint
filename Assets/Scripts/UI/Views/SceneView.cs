015443f190261a680640d69010cd61a8d68bf658
Kenneth Yang <kjy5@uw.edu>
2025-08-08 16:06:43 -0700

    Add probe from dropdown

--- Assets/Scripts/UI/Views/SceneView.cs
+++ Assets/Scripts/UI/Views/SceneView.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel;
 using UI.ViewModels;
+using Unity.AppUI.UI;
 using UnityEngine.UIElements;
 using Utils.Types;
 using Button = Unity.AppUI.UI.Button;
@@ -25,22 +26,20 @@ namespace UI.Views
             root.dataSource = sceneViewModel;
 
             // Register component references.
-            var addNeuropixels10 = root.Q<Button>("scene__add-probe-menu__neuropixels__1-0");
-            var addNeuropixels20 = root.Q<Button>("scene__add-probe-menu__neuropixels__2-0");
-            var addNeuropixels204Shank = root.Q<Button>("scene__add-probe-menu__neuropixels__2-0-4-shank");
-            var addNeuropixels2X24 = root.Q<Button>("scene__add-probe-menu__neuropixels__2x-2-4");
-            var addPipette25Um = root.Q<Button>("scene__add-probe-menu__pipette__25um");
-            var addPipette50Um = root.Q<Button>("scene__add-probe-menu__pipette__50um");
-            var addPipette100Um = root.Q<Button>("scene__add-probe-menu__pipette__100um");
-            var addPipette200Um = root.Q<Button>("scene__add-probe-menu__pipette__200um");
-            var addUcla128K = root.Q<Button>("scene__add-probe-menu__ucla__128k");
-            var addUcla256F = root.Q<Button>("scene__add-probe-menu__ucla__256f");
-            var addProbeButton = root.Q<Button>("scene__add-probe-button");
+            var addNeuropixels10 = root.Q<MenuItem>("scene__add-probe-menu__neuropixels__1-0");
+            var addNeuropixels20 = root.Q<MenuItem>("scene__add-probe-menu__neuropixels__2-0");
+            var addNeuropixels204Shank = root.Q<MenuItem>("scene__add-probe-menu__neuropixels__2-0-4-shank");
+            var addNeuropixels2X24 = root.Q<MenuItem>("scene__add-probe-menu__neuropixels__2x-2-4");
+            var addPipette25Um = root.Q<MenuItem>("scene__add-probe-menu__pipette__25um");
+            var addPipette50Um = root.Q<MenuItem>("scene__add-probe-menu__pipette__50um");
+            var addPipette100Um = root.Q<MenuItem>("scene__add-probe-menu__pipette__100um");
+            var addPipette200Um = root.Q<MenuItem>("scene__add-probe-menu__pipette__200um");
+            var addUcla128K = root.Q<MenuItem>("scene__add-probe-menu__ucla__128k");
+            var addUcla256F = root.Q<MenuItem>("scene__add-probe-menu__ucla__256f");
             _probeListView = root.Q<ListView>("scene__probe-list-view");
             _manipulatorListView = root.Q<ListView>("scene__manipulators-list-view");
 
             // Add event listeners.
-            addProbeButton.clickable.clicked += () => _sceneViewModel.AddProbeCommand.Execute(ProbeType.Neuropixels1);
             addNeuropixels10.clickable.clicked += () => _sceneViewModel.AddProbeCommand.Execute(ProbeType.Neuropixels1);
             addNeuropixels20.clickable.clicked +=
                 () => _sceneViewModel.AddProbeCommand.Execute(ProbeType.Neuropixels21);
