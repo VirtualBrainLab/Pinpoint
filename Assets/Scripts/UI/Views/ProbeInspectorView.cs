0e57df17b1874dd8b8b8e547e5f57a279eb20fbe
Kenneth Yang <kjy5@uw.edu>
2025-09-09 09:30:08 -0700

    Rename to ProbeInspector

---
+++ Assets/Scripts/UI/Views/ProbeInspectorView.cs
@@ -0,0 +1,14 @@
+using UnityEngine.UIElements;
+
+namespace UI.Views
+{
+    public class ProbeInspectorView
+    {
+        public ProbeInspectorView(TemplateContainer root)
+        {
+            // Register view model and property changes.
+            // root.dataSource = inspectorViewModel;
+            
+        }
+    }
+}
